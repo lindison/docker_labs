04afb90dbf6506a7f41806bbd3b27bc91873c766
Lindis <openstack@lindison.com>
2017-05-12 01:23:54 -0400

    updates

--- labs/swarm/build_things.sh
+++ labs/swarm/build_things.sh
@@ -1,4 +1,5 @@
-example-counter-app-stack.yml
-example-django-app-stack.yml
-example-voting-app-stack.yml
-example-wordpress-app-stack.yml
+docker stack deploy -c example-counter-app-stack.yml counter
+docker stack deploy -c example-django-app-stack.yml django
+docker stack deploy -c example-voting-app-stack.yml voteapp
+docker stack deploy -c example-wordpress-app-stack.yml wordpress
+docker service create --name hello-world --publish 81:80 lindison/hello-world:k8s --replicas 10
