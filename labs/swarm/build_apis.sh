3ebeeb4bea8e0aee60e09f4888cc0be26bf307b9
Lindis <openstack@lindison.com>
2017-05-12 01:48:29 -0400

    updates

--- labs/swarm/build_apis.sh
+++ labs/swarm/build_apis.sh
@@ -1,4 +1,4 @@
 docker service create --publish 90:8080 --name uber lindison/swagger-ui:uber
 docker service create --publish 91:8080 --name twitter lindison/swagger-ui:twitter
 docker service create --publish 92:8080 --name weather lindison/swagger-ui:weather
-docker service create --publish 82:8080 --name instagram lindison/swagger-ui:instagram
+docker service create --publish 93:8080 --name instagram lindison/swagger-ui:instagram
